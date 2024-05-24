7d99f3e34a4162475b118ac99474a7c3e27ff48b
Ajay Tripathi <tripathi.ajay12@gmail.com>
2024-05-24 17:54:10 +0530

    Added new css for responsiveness

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+// document.addEventListener('DOMContentLoaded', function(event) {
 
-    var navbarToggler = document.querySelectorAll('.navbar-toggler')[0];
-    navbarToggler.addEventListener('click', function(e) {
-      e.target.children[0].classList.toggle('active');
-    });
+//     var navbarToggler = document.querySelectorAll('.back-button')[0];
+//     navbarToggler.addEventListener('click', function(e) {
+//       e.target.children[0].classList.toggle('hide');
+//     });
 
+//   });
+
+
+$(function() {
+  $(".back-button").click(function() {
+    // $(this).toggleClass("hide");
+    $('.mobile-menu-detail-box').toggleClass("hide");
   });
+});
