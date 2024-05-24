98de1afb6d509c0cb306f9be4dff8711e620b410
Ajay Tripathi <tripathi.ajay12@gmail.com>
2024-05-24 18:28:36 +0530

    Added menu buttons on mobile size

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function(event) {
-
-//     var navbarToggler = document.querySelectorAll('.back-button')[0];
-//     navbarToggler.addEventListener('click', function(e) {
-//       e.target.children[0].classList.toggle('hide');
-//     });
-
-//   });
-
-
-// $(function() {
-//   $(".back-button").click(function() {
-//     // $(this).toggleClass("hide");
-//     $('.mobile-menu-detail-box').toggleClass("show");
-//   });
-// });
-
 $(".back-button").click(function (e) {
   e.preventDefault();
   $(this).closest(".mobile-menu-detail-box")
